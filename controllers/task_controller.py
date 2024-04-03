e44d8efb82d3d7c4228123adf60ddca3cb5b8412
Manpreet Kaur <manpreetk12012004@gmail.com>
2024-04-03 13:54:39 -0400

    Create task done.

--- controllers/task_controller.py
+++ controllers/task_controller.py
@@ -4,8 +4,35 @@ from bson.objectid import ObjectId
 import app_config as config
 
 # TODO: Manpreet Kaur
-def create_task():
-    pass
+def create_task(task_info):
+    try:
+        # Access the database to get user information
+        created_by_user = conn.database[config.CONST_USER_COLLECTION].find_one({'_id': ObjectId(task_info['created_by_uid'])})
+        assigned_to_user = conn.database[config.CONST_USER_COLLECTION].find_one({'_id': ObjectId(task_info['assigned_to_uid'])})
+        
+        # print("created_by_user", created_by_user)
+        # print("assigned_to_user", assigned_to_user)
+        
+        if not created_by_user or not assigned_to_user:
+            raise ValueError('Invalid user information')
+        
+        # Create a new task object
+        new_task = Task()
+        new_task.createdByUid = task_info['created_by_uid']
+        new_task.createdByName = created_by_user['name']
+        new_task.assignedToUid = task_info['assigned_to_uid']
+        new_task.assignedToName = assigned_to_user['name']
+        new_task.description = task_info['description']
+        
+        # Save the task to the database
+        created_task = conn.database[config.CONST_TASK_COLLECTION].insert_one(new_task.__dict__)
+        
+        return created_task
+    
+    except ValueError as err:
+        raise ValueError(str(err))
+    except Exception as err:
+        raise ValueError(str(err))
 
 # TODO: Dil Raval
 def get_task_created_by_user():
--- views/task_view.py
+++ views/task_view.py
@@ -7,6 +7,39 @@ from controllers.task_controller import create_task
 task = Blueprint("task", __name__)
 
 # TODO: Manpreet Kaur. Create task route.
+@task.route("/tasks/", methods=["POST"])
+def createTask():
+    try:
+        # Validate JWT token
+        user_info = validate_jwt()
+        
+        if user_info == 400:
+            return jsonify({"error": 'Token is missing in the request, please try again'}), 401
+        elif user_info == 401:
+            return jsonify({"error": 'Invalid authentication token, please login again'}), 403
+        
+        # Checking if required keys are in the request data
+        data = request.json
+        if 'description' not in data or 'assignedToUid' not in data:
+            raise ValueError('Error validating information')
+        
+        # Extracting the information from request data
+        description = data['description']
+        assigned_to_uid = data['assignedToUid']
+        created_by_uid = user_info['id']  # Extract user ID from token
+        
+        # Create a dictionary with task information
+        task_info = {'description': description, 'assigned_to_uid': assigned_to_uid, 'created_by_uid': created_by_uid}
+        
+        # Call the create_task_controller function with task_info
+        created_task = create_task(task_info)
+        
+        # Return the ID of the created task
+        return jsonify({'id': str(created_task.inserted_id)})
+    
+    except ValueError as err:
+        return jsonify({"error": str(err)}), 400
+
 
 # TODO: Dil Raval. Get tasks created by the user route.
 
