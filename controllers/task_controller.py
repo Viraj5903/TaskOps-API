5fa5572e776d274da76232a86a9810c94eb7f767
Payal <payalrangra04@gmail.com>
2024-04-04 15:25:11 -0400

    Update using Patch , without comments.

--- controllers/task_controller.py
+++ controllers/task_controller.py
@@ -16,10 +16,23 @@ def get_tasks_assigned_to_user():
     pass
 
 # TODO: Payal Rangra
-def update_task():
+def update_task(user_info , task_id ,  done ):
     
-    #Hi , new code 
-    pass
+    try:
+        task_collection=conn.database[config.CONST_TASK_COLLECTION]
+        
+        current_task = task_collection.find_one({"_id":ObjectId(task_id)})
+        if(current_task==None):  
+            return "No such task exists."
+        
+        if str(current_task['assignedToUid'])!=str(user_info["id"]):
+            raise ValueError('Users can only change status when task is assigned to them.')
+        
+        updated_result = task_collection.update_one({"_id":ObjectId(task_id)}, {"$set": {"done": done}})
+        
+        return updated_result
+    except Exception as err:
+        raise ValueError("Error on updating task.", err)
 
 # TODO: Viraj Patel
 def delete_task():
--- views/task_view.py
+++ views/task_view.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, jsonify, request
 import json
 from helpers.token_validation import validate_jwt
-from controllers.task_controller import create_task
+from controllers.task_controller import create_task ,update_task
 
 # task = Blueprint("task", __name__, url_prefix="/tasks/")
 task = Blueprint("task", __name__)
@@ -13,7 +13,35 @@ task = Blueprint("task", __name__)
 # TODO: Aryan Handa. Get tasks assigned to the user route.
 
 # TODO: Payal Rangra. Update task route.
+@task.route("/tasks/<taskUid>", methods=["PATCH"])
+def updatetask(taskUid):
+    try:    
+        # Validating the user.
+        token = validate_jwt() 
 
+        if token == 400:
+            return jsonify({"error": 'Token is missing in the request, please try again'}), 401
+        if token == 401:
+           return jsonify({"error": 'Invalid authentication token, please login again'}), 403
+        
+        
+        
+        data = json.loads(request.data)
+        if  "done" not in data:
+            return jsonify({"error": 'Status done not found in the request'}), 400
+       
+        result = update_task(token, taskUid, data["done"])
+        
+        if result=="No such task exists." :
+            return jsonify({"Error":"Task does not exist"}), 404
+        
+        return  jsonify({"taskUid":taskUid}) , 200
+
+    except ValueError as error:
+        return jsonify({'error': f'Error on updating  the task. {error}'}), 500
+    except Exception as error:
+        return jsonify({'error': error}), 500
+    
 # TODO: Viraj Patel. Delete task route.
 @task.route("/tasks/<taskUid>", methods=["DELETE"])
 def deleteTask(taskUid):
@@ -29,7 +57,7 @@ def deleteTask(taskUid):
         
         user_information = token
 
-    except ValueError:
+    except ValueError as error:
         return jsonify({'error': 'Error creating task.'}), 500
     except Exception as error:
         return jsonify({'error': error}), 500
